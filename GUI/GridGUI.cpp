857c164efebed85ecf770bc129a683683dca2525
mcmeaties4 <ophil_iv@hotmail.com>
2015-04-15 00:10:08 +1000

    this too

--- GUI/GridGUI.cpp
+++ GUI/GridGUI.cpp
@@ -1,6 +1,10 @@
-using namespace environment; 
+#define pixels_per_cell 25
+#include "GridGUI.h"
+#include "WinBGImPainter.h"
+#include <exception>
+using namespace environment;
 
-gridGUI::gridGUI(Grid* paintgrid, String name, int screenLeft)
+GridGUI::GridGUI(Grid* paintgrid, char* name, int screenLeft)
 	{
 //colour defines
             Grid_line= Colour(0,0,0);
@@ -11,59 +15,95 @@ gridGUI::gridGUI(Grid* paintgrid, String name, int screenLeft)
             Robot= Colour(200,1,1);
 
 	grid=paintgrid;
-		
+
 //size define
-            windowHeight=pixels_per_cell*(grid->getLast().getRow()+1);
+    windowHeight=pixels_per_cell* (grid->getLast().getRow()+1);
 	windowWidth=pixels_per_cell*(grid->getLast().getColumn()+1);
-	
-//make the painter and such
+
+    //make the painter and such
 	painter=  new WinBGImPainter (grid->getLast().getColumn()+1, grid->getLast().getRow()+1);
-	
-	window= initwindow(grid->getLast().getColumn()+1, grid->getLast().getRow()+1, namee ,screenLeft);
+	window= initwindow(grid->getLast().getColumn()+1, grid->getLast().getRow()+1, name ,screenLeft);
 	delay(100);
         }
 
-void gridGUI::updateSize(Grid* paintgrid)
+void GridGUI::updateSize(Grid* paintgrid)
 {
 	grid= paintgrid;
 	windowHeight=pixels_per_cell*(grid->getLast().getRow()+1);
         windowWidth=pixels_per_cell*(grid->getLast().getColumn()+1);
 }
 
-void gridGUI::paint()
+void GridGUI::paint()
 {
-	
+
 	if(getcurrentwindow( )!=window)
      	{
         	 setcurrentwindow(window);
      	}
 
-	
+
 	for(unsigned i=0; i<grid->getLast().getRow();i++)
      	{
 
          	for(unsigned j=0; j<grid->getLast().getColumn();j++)
          	{
-			bool update =false;
-			
-			std::vector<Content> newContentList = grid->getContent() ;
-			try
-			{
-				std::vector<Content> oldContnetList = oldGrid.getContnet();
-			}
-			catch(Exception e){std::cout<<e.}
-			
-			for(unsigned i=0; i<newContentList.size()			
+                bool update =false;
+                Grid newGrid= *grid;
+                std::vector<Content> newContentList = newGrid[Coordinate(i,j)].getContent() ;
+                std::vector<Content> oldContnetList;
+                try
+                {
+                     oldContnetList = oldGrid[Coordinate(i,j)].getContent();
+                }
+                catch(...){std::cout<<"OldGrid exep"<<std::endl;}
 
-			if(oldGrid=null || !oldGrid[]
-           		//draw grid cell
-          		painter->setPenColour(Grid_line);
-           		painter->setFillColour(Grid_fill);
-           		painter->drawSquare(Pixel((j+1)*pixels_per_cell,(i+1)*pixels_per_cell), pixels_per_cell);
+                for(unsigned i=0; i<oldContnetList.size(); i++)
+                {
+                    //look if somthing removed
+                    if(!newGrid[Coordinate(i,j)].hasContent(oldContnetList[i]))
+                    {
+                        update=true;
+                        break;
+                    }
+
+                }
+                 for(unsigned i=0; i<newContentList.size(); i++)
+                {
+                    //look if somthing added
+                     if(!oldGrid[Coordinate(i,j)].hasContent(newContentList[i]))
+                    {
+                        update=true;
+                        break;
+                    }
+                }
+
+                if(update)
+           		{
+           		    //draw grid cell
+                    painter->setPenColour(Grid_line);
+                    painter->setFillColour(Grid_fill);
+                    painter->drawSquare(Pixel((j+1)*pixels_per_cell,(i+1)*pixels_per_cell), pixels_per_cell);
+
+                    if(newGrid[Coordinate(i,j)].hasContent(ContentType::Goal))
+                    {
+                        painter->setPenColour(Goal);
+                        painter->setFillColour(Goal);
+                        painter->drawCircle(Pixel(float(j-0.5)*pixels_per_cell,float(i-0.5)*pixels_per_cell), pixels_per_cell/4);
+                    }
+
+                    if(newGrid[Coordinate(i,j)].hasContent(ContentType::Robot))
+                    {
+                        painter->setPenColour(Robot);
+                        painter->setFillColour(Robot);
+                        painter->drawCircle(Pixel(float(j+0.5)*pixels_per_cell,float(i+0.5)*pixels_per_cell), pixels_per_cell/4);
+                    }
+
+
+           		}
         	}
    	 }
 
-	oldGrid =*grid;//make this my old grid so next paint knows what to chage
+	oldGrid = *grid;//make this my old grid so next paint knows what to chage
 
 }
 
---
+++ Relay/ImplementRelay.cpp
@@ -0,0 +1,8 @@
+#include "ImplementRelay.h"
+using namespace Relayspace;
+
+bool ImplementRelay::inRange(Coordinate test)
+{
+    std::cout<<"no in Range"<<std::endl;
+    return false;
+}
