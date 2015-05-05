78071d60e6d0de6feec3731aa79c6ca7958f584d
ismash <marshalltsimons@gmail.com>
2015-05-05 10:58:52 +1000

    Added new diagonal directions to enum Direction, general formatting

--- Agent/Node.h
+++ Agent/Node.h
@@ -8,30 +8,35 @@ using namespace environment;
 namespace Agentspace{
  class Node{
 public:
-        ///direcections robot has move
-        enum Direction {Right, Left, Up, Down, Root};//Root means did not move to get there, as at ....root position
+    ///direcections robot has move
+    enum Direction{
+        Right,
+        Left,
+        Up,
+        Down,
+        Up-Right,
+        Up-Left,
+        Down-Right,
+        Down-Left,
+        Root };//Root means did not move to get there, as at ....root position
 
-      Node(Coordinate _state);
-Node(Node* _Parent, Coordinate _state, Direction _actionToReach);
+    Node(Coordinate _state);
+    Node(Node* _Parent, Coordinate _state, Direction _actionToReach);
 
+    Node operator = (Node was);
 
-      Node operator = (Node was);
+    //gets
+    inline const Coordinate getState() const {return state;}
+    inline const unsigned getLevel() const {return level;}
+    inline  Node* getParent() const {return Parent;}
+    inline const Direction getAction() const {return actionToReach;}
+    inline const unsigned getHeuristic() const {return heuristic;}
+    inline const unsigned getChildren() const {return children;}
 
-      //gets
-        inline const Coordinate getState() const {return state;}
-         inline const unsigned getLevel() const {return level;}
-         inline  Node* getParent() const {return Parent;}
-         inline const Direction getAction() const {return actionToReach;}
-        inline const unsigned getHeuristic() const {return heuristic;}
-      inline const unsigned getChildren() const {return children;}
-
-
-         void setHeuristic(Coordinate goal);
-         inline void Broken_condom(){children++;}
-         inline void slender_man(){children--;}
- void deadEnd(Grid& grid,  list<Node*>* Leaves);
-
-
+    void setHeuristic(Coordinate goal);
+    inline void Broken_condom(){children++;}
+    inline void slender_man(){children--;}
+    void deadEnd(Grid& grid,  list<Node*>* Leaves);
 
 private:
     Node* Parent;   //the node that made this node
@@ -40,9 +45,6 @@ private:
     Direction actionToReach; //what does the robot need to do to reach this state
     unsigned heuristic;   //how good this node is for reaching goal
 	unsigned children; //used for freeing useless nodes
-
-
-
 };
 }
 #endif
